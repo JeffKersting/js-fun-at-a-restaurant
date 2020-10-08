13e93b7261d99389e4ccb0c5316d097844115170
Jeff Kersting <jeff.w.kersting@gmail.com>
2020-10-08 16:29:30 -0600

    Save meal.js functions

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,35 @@
-function nameMenuItem() {
+function nameMenuItem(foodItem) {
+  return `Delicious ${foodItem}`
+}
+
+function createMenuItem(name,price,type){
+  var menuItem = {
+    name: name,
+    price: price,
+    type: type
+  }
+  return menuItem
+}
+
+function addIngredients(ingredient, ingredients){
+  if(ingredients.includes(ingredient) == true){
+    return ingredients;
+  } else {
+  ingredients.push(ingredient)
+  return ingredients
+  }
 
 }
 
+function formatPrice() {
+  
+}
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
+  createMenuItem,
+  addIngredients,
   // formatPrice,
   // decreasePrice,
   // createRecipe
 }
-
-
