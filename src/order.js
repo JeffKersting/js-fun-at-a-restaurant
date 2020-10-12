c3492f54c1a2463ac5ef89b6ac0235eb2d9e153c
Jeff Kersting <jeff.w.kersting@gmail.com>
2020-10-11 20:57:40 -0600

    Upload test cases and functions

--- src/order.js
+++ src/order.js
@@ -1,7 +1,41 @@
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+  takeOrder,
+  refundOrder,
+  listItems,
+  searchOrder
+}
+
+function takeOrder(order, deliveryOrders){
+  if(deliveryOrders.length === 3){
+    return
+  } else {
+  deliveryOrders = deliveryOrders.push(order)
+  return deliveryOrders
+  }
+}
+
+function refundOrder(orderNum, deliveryOrders) {
+  for(i = 0; i < deliveryOrders.length; i++){
+    if(deliveryOrders[i].orderNumber === orderNum){
+    deliveryOrders.splice(i , 1)
+    }
+  }
+  return deliveryOrders
+}
+
+function listItems(deliveryOrders){
+  var listedItems = []
+  for(i = 0; i < deliveryOrders.length; i++){
+    listedItems.push(deliveryOrders[i].item)
+  }
+  return `${listedItems[0]}, ${listedItems[1]}, ${listedItems[2]}`
+}
+
+function searchOrder(deliveryOrders, foodType){
+  var foodOrders = []
+  for(i = 0; i < deliveryOrders.length; i++){
+    foodOrders.push(deliveryOrders[i].item)
+    }
+    return foodOrders.includes(foodType)
 }
--- src/restaurant.js
+++ src/restaurant.js
@@ -1,7 +1,48 @@
 
 
 module.exports = {
-  // createRestaurant, 
-  // addMenuItem,
-  // removeMenuItem
+  createRestaurant,
+  addMenuItem,
+  removeMenuItem
 }
+function createRestaurant(name, menus){
+  var restaurant = {
+    name: name,
+    menus: {
+      breakfast: [],
+      lunch: [],
+      dinner: []
+    }
+  }
+  return restaurant
+}
+
+function addMenuItem(restaurant, menuItem){
+
+  if(menuItem.type === "breakfast" && restaurant.menus.breakfast.includes(menuItem) === false){
+    restaurant.menus.breakfast.push(menuItem)
+  } else if(menuItem.type === "lunch" && restaurant.menus.lunch.includes(menuItem) === false){
+    restaurant.menus.lunch.push(menuItem)
+  } else if(menuItem.type === "dinner" && restaurant.menus.dinner.includes(menuItem) === false){
+    restaurant.menus.dinner.push(menuItem)
+  } else {
+    return
+  }
+  return restaurant
+}
+
+function removeMenuItem(restaurant, foodName, menuType){
+    if(menuType === 'breakfast' && restaurant.menus.breakfast[0].name === foodName){
+        restaurant.menus.breakfast = []
+      }
+    else if(menuType === 'lunch' && restaurant.menus.lunch[0].name === foodName){
+        restaurant.menus.lunch = []
+      }
+    else if(menuType === 'dinner' && restaurant.menus.dinner[0].name === foodName){
+        restaurant.menus.dinner = []
+      }
+    else{
+      return `Sorry, we don't sell ${foodName}, try adding a new recipe!`
+    }
+    return `No one is eating our ${foodName} - it has been removed from the ${menuType} menu!`
+  }
